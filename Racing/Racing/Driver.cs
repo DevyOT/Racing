8a6e604b51acaa9dc7555e275637b580f051e1ed
xepoman <ivan.egov@mail.ru>
2019-04-17 12:28:17 +0700

    Update Fix#2

--- Racing/Racing/Driver.cs
+++ Racing/Racing/Driver.cs
@@ -20,19 +20,16 @@ namespace Racing
         /// скорость. 
         /// </summary>
 
-        public double speedDrave;
+        public double DriverCoef;
 
         int exp = 0;
-        int quantityVictory = 0;
-        int quantityDefeat = 0;
+        int win = 0;
+        int lose = 0;
        
         bool isDrunk = false;
         bool driverLicence = true;
 
         string name;
-
-        double driverLicenceCoef;
-        double drunkPromili;
 
         public Driver(string name, bool isDrunk, bool driverLicence)
         {
@@ -40,14 +37,14 @@ namespace Racing
             this.name = name;
             this.isDrunk = isDrunk;
             this.driverLicence = driverLicence;
-            this.speedDrave = CalcCoef();
+            this.DriverCoef = CalcCoef();
         }
 
-        public double CalcCoef()
+        private double CalcCoef()
         {
             /* возвращает коэфициент водителя */
-            driverLicenceCoef = driverLicence == true ? 0.5 : 0.1;
-            drunkPromili = isDrunk == true ? 0.5 : 0.1;
+            double driverLicenceCoef = driverLicence == true ? 0.5 : 0.1;
+            double drunkPromili = isDrunk == true ? 0.5 : 0.1;
 
             /* возвращает коэфициент водителя */
             switch (name)
@@ -56,17 +53,13 @@ namespace Racing
                 case "Ivan": return 0.3 + driverLicenceCoef + drunkPromili;
                 case "Igor": return 0.4 + driverLicenceCoef + drunkPromili;
                 case "Petr": return 0.5 + driverLicenceCoef + drunkPromili;
+                default: return 0.1;
             }
-            return 0.1;
         }
         public string GetDriverInfo()
         {
-
             /* возвращает данные о водителе */
-            string drunkStr = isDrunk == true ? "бухой" : "трезвый";
-            string driverLicenceStr = driverLicence == true ? "ecть" : "отсутствуют";
-            
-            return $"Имя: {name}.  Опыт: {exp} Кол-во побед: {quantityVictory} Кол-во порожений: {quantityDefeat}  Водитель {drunkStr}. Права {driverLicenceStr}.";
+            return $"Опыт: {exp} Кол-во побед: {win} Кол-во поражений: {lose}";
         }
     }
 }
